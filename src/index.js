28207feb2e99f0e279208f1086118268df3bdfc1
sarahmr63089 <sarahrose63089@gmail.com>
2020-08-07 14:16:02 -0400

    Done.

---
+++ src/index.js
@@ -0,0 +1,123 @@
+let dogBar = document.querySelector("#dog-bar")
+let dogShow = document.querySelector("#dog-info")
+let filterDogs = document.querySelector("#good-dog-filter")
+let filter = false
+
+let allDogFetch = () => {
+  fetch("http://localhost:3000/pups")
+  .then(res => res.json())
+  .then((pupObjArray) => {
+    pupObjArray.forEach((pupObj) => {
+      turnPupIntoHTML(pupObj)
+    })
+  })
+}
+
+allDogFetch()
+
+let turnPupIntoHTML = (pupObj) => {
+  // { id: num, name: name, isGoodDog: boolean, image: url }
+
+  let dogSpan = document.createElement("span")
+  dogSpan.innerText = pupObj.name
+  dogSpan.className = pupObj.isGoodDog
+
+  dogBar.append(dogSpan)
+
+  let pupImg = document.createElement("img")
+
+  let pupNameH2 = document.createElement("h2")
+
+  let goodPupButton = document.createElement("button")
+
+  dogSpan.addEventListener("click", (evt) => {
+    if (dogShow.children.length > 0) { 
+      dogShow.children[0].src = pupObj.image
+      dogShow.children[1].innerText = pupObj.name
+      if (pupObj.isGoodDog) { 
+        dogShow.children[2].innerText = "Good Dog!" 
+      }
+      else { 
+        dogShow.children[2].innerText = "Bad Dog!" 
+      }
+    }
+    else {
+      pupImg.src = pupObj.image
+      pupNameH2.innerText = pupObj.name
+      if (pupObj.isGoodDog) { 
+        goodPupButton.innerText = "Good Dog!" 
+      }
+      else { 
+        goodPupButton.innerText = "Bad Dog!" 
+      }
+      dogShow.append(pupImg, pupNameH2, goodPupButton)
+    }
+  })
+
+  goodPupButton.addEventListener("click", (evt) => {
+    // Update memory, update database, update page
+    pupObj.isGoodDog = !pupObj.isGoodDog
+
+    fetch(`http://localhost:3000/pups/${pupObj.id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        isGoodDog: pupObj.isGoodDog
+      })
+    })
+    .then(res => res.json())
+    .then((updatedPup) => {
+      if (updatedPup.isGoodDog) { 
+        evt.target.innerText = "Good Dog!" 
+      }
+      else { 
+        evt.target.innerText = "Bad Dog!" 
+      }
+      if (filter) {
+        dogBar.innerHTML = ""
+
+        fetch("http://localhost:3000/pups")
+        .then(res => res.json())
+        .then((pupObjArray) => {
+          let pupArr = pupObjArray.filter((pup) => pup.isGoodDog) 
+
+          pupArr.forEach((pupObj) => { 
+            turnPupIntoHTML(pupObj)
+          })
+        })
+      }
+    })
+  })
+}
+
+filterDogs.addEventListener("click", (evt) => {
+  filter = !filter
+  
+  if (filter) {
+    evt.target.innerText = "Filter good dogs: ON"
+
+    dogBar.innerHTML = ""
+    
+    fetch("http://localhost:3000/pups")
+    .then(res => res.json())
+    .then((pupObjArray) => {
+      let pupArr = pupObjArray.filter((pup) => pup.isGoodDog) 
+
+      pupArr.forEach((pupObj) => { 
+        turnPupIntoHTML(pupObj)
+      })
+    })
+  }
+
+  if (!filter) {
+      
+    evt.target.innerText = "Filter good dogs: OFF"
+
+    dogBar.innerHTML = ""
+    
+    allDogFetch()
+  }
+})
+
